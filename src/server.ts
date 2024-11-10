29d4f4cfc2376bec62ba5f1908eabaf14e6748a3
pheranmie <okekunletobi@gmail.com>
2024-11-10 14:12:10 +0100

    removed

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,6 @@
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
 import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
-import { ApolloServerPluginLandingPageDisabled } from "@apollo/server/plugin/disabled";
 import express from "express";
 import http from "http";
 import cors from "cors";
@@ -86,9 +85,7 @@ const startServer = async () => {
     typeDefs,
     resolvers,
     plugins: [
-      ApolloServerPluginLandingPageDisabled(),
-      ApolloServerPluginDrainHttpServer({ httpServer }),
-    ],
+      ApolloServerPluginDrainHttpServer({ httpServer })],
   });
 
   await server.start();
